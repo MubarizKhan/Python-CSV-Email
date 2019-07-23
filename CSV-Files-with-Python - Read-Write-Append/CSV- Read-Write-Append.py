a3bb43e76fce2c31bab9855e408c175df2d3a837
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-07-24 01:41:51 +0500

    Add files via upload

---
+++ CSV-Files-with-Python - Read-Write-Append/CSV- Read-Write-Append.py
@@ -0,0 +1,34 @@
+import csv
+
+with open("data.csv","w+") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Title","Description"])
+    writer.writerow(["row 1","Descript"])
+    writer.writerow(["row 2","Description"])
+    writer.writerow(["row 3","Descript"])
+
+"a" Append to a file--- add to the very end
+"w" writes to the file, w+ overwrites to the file
+"r" read only
+"r+" read & write
+"rb" read in binary form
+
+with open("data.csv","r") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+
+with open("data.csv","a") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["hi", "my name is","johnny doe"])
+    writer.writerow(["hi", "my name is","johnny doe"])
+    writer.writerow(["hi", "my name is","johnny doe"])
+    writer.writerow(["hi", "my name is","johnny doe"])
+
+with open("data.csv","r") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in csv.DictReader(csvfile):
+        print(row)
+
+
